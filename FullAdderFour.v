0d06edc7c054b5efd56f51df8779182739f8ff94
Clouds42 <1577338630@qq.com>
2019-10-13 19:51:09 +0800

    v1.1
    最终版,加入led指示灯,修复若干bug

--- FullAdderFour.v
+++ FullAdderFour.v
@@ -1,16 +1,20 @@
-module FullAdderFour(input clk,input rst,input key_a,input key_b,input key_cal,input[3:0]sw_dig,output[8:0]seg_rt,output[8:0]seg_lt);
+module FullAdderFour(input clk,input rst,input key_a,input key_b,input key_cal,input[3:0]sw_dig,output[8:0]seg_rt,output[8:0]seg_lt,output[2:0]led);
 
 	reg[8:0]seg[15:0];//数码管显示数据库.
 	reg[3:0]add1;//加数一.
 	reg[3:0]add2;//加数二.
 	reg[7:0]sum;//和
 	reg[3:0]seg_data[1:0];//数码管显示数据流.
+	reg led_data[2:0];
 
 	wire[3:0]add1_pcd;
 	wire[3:0]add2_pcd;
-	wire c_c1;
 	wire[7:0]sum_pcd;
+	wire c_c1;
 	wire[3:0]c_c2;
+	wire key_a_dbs;
+	wire key_b_dbs;
+	wire key_cal_dbs;
 
 	assign add1_pcd = add1;
 	assign add2_pcd = add2;
@@ -29,23 +33,35 @@ module FullAdderFour(input clk,input rst,input key_a,input key_b,input key_cal,i
 		seg[9] <= 9'h6f;
 		seg[10] <= 9'h40;
 		sum <= 0;
+		led_data[0] <= 1;
+		led_data[1] <= 1;
+		led_data[2] <= 1;
 	end
 
 	always@(posedge clk or negedge rst)begin
 		if(!rst)begin
 			seg_data[0] <= 10;
 			seg_data[1] <= 10;
+			led_data[0] <= 1;
+			led_data[1] <= 1;
+			led_data[2] <= 1;
 			sum <= 0;
 		end
 		else if(!key_a)begin
 			add1 <= sw_dig;
+			led_data[0] <= 0;
+			led_data[1] <= 1;
+			led_data[2] <= 1;
 			if(add1 > 9)
-				{seg_data[1],seg_data[0]} <= add1 + 4'd6;
+				{seg_data[1],seg_data[0]} <= add1 + 4'd6;//加六修正.
 			else
 				seg_data[0] <= add1;
 		end
 		else if(!key_b)begin
 			add2 <= sw_dig;
+			led_data[0] <= 0;
+			led_data[1] <= 0;
+			led_data[2] <= 1;
 			if(add2 > 9)
 				{seg_data[1],seg_data[0]} <= add2 + 4'd6;
 			else
@@ -53,6 +69,9 @@ module FullAdderFour(input clk,input rst,input key_a,input key_b,input key_cal,i
 		end
 		else if(!key_cal)begin
 			sum<=sum_pcd;
+			led_data[0] <= 1;
+			led_data[1] <= 1;
+			led_data[2] <= 0;
 			if(sum > 29)
 				{seg_data[1],seg_data[0]} <= sum + 8'd18;
 			else if(sum > 19)
@@ -62,7 +81,7 @@ module FullAdderFour(input clk,input rst,input key_a,input key_b,input key_cal,i
 			else
 				{seg_data[1],seg_data[0]} <= sum;
 		end
-		else if(!sum)begin
+		else if(!sum)begin//当还没有进行计算的时候让数码管动态显示当前数值.
 			if(sw_dig < 10)begin
 				seg_data[0] <= sw_dig;
 				seg_data[1] <= 0;end
@@ -80,7 +99,31 @@ module FullAdderFour(input clk,input rst,input key_a,input key_b,input key_cal,i
 		.c2(c_c2)
 		);
 
-	assign seg_rt=seg[seg_data[0]];
-	assign seg_lt=seg[seg_data[1]];
+	debounce BD1(
+		.clk(clk),
+		.rst(rst),
+		.key(key_a),
+		.key_pulse(key_a_dbs)
+		);
+
+	debounce BD2(
+		.clk(clk),
+		.rst(rst),
+		.key(key_b),
+		.key_pulse(key_b_dbs)
+		);
+
+	debounce BD3(
+		.clk(clk),
+		.rst(rst),
+		.key(key_cal),
+		.key_pulse(key_cal_dbs)
+		);
+
+	assign seg_rt = seg[seg_data[0]];
+	assign seg_lt = seg[seg_data[1]];
+	assign led[0] = led_data[0];
+	assign led[1] = led_data[1];
+	assign led[2] = led_data[2];
 
 endmodule
